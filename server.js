7467c1893e54b0af09fecdf85425bdc37b540e17
BRosencrans <brandon.rosencrans@gmail.com>
2023-01-29 12:35:30 -0800

    added server routes and express functionality

--- server.js
+++ server.js
@@ -7,6 +7,25 @@ const app = express()
 //Sets server port 
 const PORT = process.env.PORT || 3000
 
+//allows express to parse asset data for the server
+app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+//server route to the main page
+app.get('/', (req, res) => {
+    console.log(__dirname)
+    res.sendFile(path.join(__dirname, 'public/index.html'))
+})
+//server route to the notes.html
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public/notes.html'))
+})
+//server route to the db.json so all the note data can be read
+app.get('/api/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, 'db/db.json'))
+})
+
 //shows you what port is in use
 app.listen(PORT, () => {
     console.log(`Server listening in on http://localhost:${PORT}`)
