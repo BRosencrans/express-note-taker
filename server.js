125ae11d0e0a8087dd5e045b1b8a848e846dde3e
BRosencrans <brandon.rosencrans@gmail.com>
2023-01-29 14:26:18 -0800

    Added express function to create new notes

--- server.js
+++ server.js
@@ -26,6 +26,17 @@ app.get('/api/notes', (req, res) => {
     res.sendFile(path.join(__dirname, 'db/db.json'))
 })
 
+//express function that allows a new note to be created and stored in the db.json and displayed to end user
+app.post("/api/notes", (req, res) => {
+    const newText= req.body;
+    const textList = JSON.parse(fs.readFileSync("./db/db.json", "utf8"));
+    
+    //writes and pushes note data to db.json
+    textList.push(newText);
+    fs.writeFileSync("./db/db.json", JSON.stringify(textList));
+    res.json(textList);
+})
+
 //shows you what port is in use
 app.listen(PORT, () => {
     console.log(`Server listening in on http://localhost:${PORT}`)
