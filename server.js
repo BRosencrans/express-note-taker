0b1f494d61c6ad69a8c3db82eefdc0360099247d
BRosencrans <brandon.rosencrans@gmail.com>
2023-01-29 16:31:09 -0800

    added delete fuctionality for old notes

--- server.js
+++ server.js
@@ -37,7 +37,18 @@ app.post("/api/notes", (req, res) => {
     fs.writeFileSync("./db/db.json", JSON.stringify(textList));
     res.json(textList);
 })
-
+//deletes notes according to their id number
+app.delete("/api/notes/:id", (req, res) => {
+    let textList= JSON.parse(fs.readFileSync("./db/db.json", "utf8"));
+    let TextId = (req.params.id).toString();
+    //filters notes based on their id number
+    textList = textList.filter(selected =>{
+        return selected.id != TextId;
+    })
+    //updates the db.json and deletes note
+    fs.writeFileSync("./db/db.json", JSON.stringify(textList));
+    res.json(textList);
+});
 //shows you what port is in use
 app.listen(PORT, () => {
     console.log(`Server listening in on http://localhost:${PORT}`)
